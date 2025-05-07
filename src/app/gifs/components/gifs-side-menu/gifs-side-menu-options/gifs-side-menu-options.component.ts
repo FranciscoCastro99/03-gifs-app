0ed368f9bbfd0b4ef297705672bfabfdddce6997
FranciscoCastro <franciscocastro0399@gmail.com>
2025-05-07 15:10:39 -0500

    trending-page

---
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption{
+  label: string;
+  sublabel: string;
+  route: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
+  templateUrl: './gifs-side-menu-options.component.html',
+})
+export class GifsSideMenuOptionsComponent {
+
+  menuOption:MenuOption[] = [
+    {
+      icon:'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon:'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ]
+
+ }
