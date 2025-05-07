008d8ac303a07c30628a7afd75835225bb68c8df
FranciscoCastro <franciscocastro0399@gmail.com>
2025-05-07 12:10:51 -0500

    ejercicio corregido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import SearchPageComponent from "./gifs/pages/search-page/search-page.component";
+import DashboardPageComponent from "./gifs/pages/dashboard-page/dashboard-page.component";
+import TrendingPageComponent from "./gifs/pages/trending-page/trending-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import DashboardPageComponent from './gifs/pages/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path:'trending',
+    loadComponent: () =>
+      import('./gifs/pages/trending-page/trending-page.component'),
+  },
+  {
+    path:'search',
+    loadComponent: () =>
+      import('./gifs/pages/search-page/search-page.component'),
+  },
+  {
+    path:'**',
+    redirectTo: 'dashboard',
+  },
+  //NO SE PUEDE DEJAR { } vacias porque sino no carga las rutas
+];
